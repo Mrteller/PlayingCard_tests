771b655fbe05baa59bb228baeaaa1a1bf5d25603
Pav Tolmach <pav.tolmach@gmail.com>
2018-06-27 12:43:25 +0300

    Beginning of L8

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,72 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     var deck = PlayingCardDeck()
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left, .right]
-            //swipe.numberOfTouchesRequired = 1
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: self, action: #selector(adjustFaceCardScale (byHandlingGestureRecognizedby: )))
-            playingCardView.addGestureRecognizer(pinch)
-            
-        }
-    }
-    
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default:
-            break
-        }
 
-    }
+
+    @IBOutlet var cardViews: [PlayingCardView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       /*
-        for i in 0..<deck.cards.count {
-            if let card = deck.draw() {
-                print("\(card) - \(i)")
-            }
-        }*/
-    }
-    
-    @objc func nextCard(){
-        print("swiped")
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-            
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count+1)/2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        print("\(cards)")
+        for cardView in cardViews {
+            cardView.isFaceUp = true
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
         }
     }
-    
-    @objc func adjustFaceCardScale(byHandlingGestureRecognizedby recognizer: UIPinchGestureRecognizer) {
-        switch recognizer.state {
-        case .changed, .ended:
-            playingCardView.faceCardScale *= recognizer.scale
-            recognizer.scale = 1.0
-            // playingCardView.faceCardScale = recognizer.scale * playingCardView.SizeRatio.faceCardImageSizeToBoundsSize
-        default:
-            break
-        }
-    }
-// Playing with Date and DateFormatter - delete it
-    private func t() {
-        let someDate = Date(timeIntervalSinceReferenceDate: 0)
-        let dFormatter = DateFormatter()
-        dFormatter.dateStyle = .full
-        var dateString = dFormatter.string(from: someDate)
-        for dateStyle in DateFormatter.Style.full.rawValue...DateFormatter.Style.short.rawValue  {
-            dFormatter.dateStyle = DateFormatter.Style.init(rawValue: dateStyle) ?? .none
-            dateString = dFormatter.string(from: someDate)
-            print(dateString)
-        }
-        
-        //let dds = Set(DateFormatter.Style)
-    }
-    
     
 
 }
